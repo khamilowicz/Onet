81709a608c98f0b9328c8169685e8109e8687786
khamilowicz <piotr.szeremeta@gmail.com>
2012-09-06 17:16:00 +0200

    n

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,20 +7,39 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
+#10.times do
+  #a = Article.new(
+    #{
+    #title: Faker::Lorem.sentence, 
+    #content: Faker::Lorem.paragraphs(Random.rand(10..20)),
+    #image_path: '/'
+  #})
+  #a.popularity = Random.rand(0..12)
+  #a.save
+  #Random.rand(0..10).times do
+    #a.comments.create(
+      #{
+        #title: Faker::Lorem.sentence,
+        #content: Faker::Lorem.sentences(Random.rand(2..12))
+      #}
+    #)
+  #end
+
+#end
 10.times do
   a = Article.new(
     {
     title: Faker::Lorem.sentence, 
-    content: Faker::Lorem.paragraphs(Random.rand(10..20)),
+    content: Faker::Lorem.paragraphs(5),
     image_path: '/'
   })
-  a.popularity = Random.rand(0..12)
+  a.popularity = 5
   a.save
-  Random.rand(0..10).times do
+  10.times do
     a.comments.create(
       {
         title: Faker::Lorem.sentence,
-        content: Faker::Lorem.sentences(Random.rand(2..12))
+        content: Faker::Lorem.sentences(5)
       }
     )
   end
